e7165072d78201175598e4949a53880f9ef1f6d7
savoca <adeddo27@gmail.com>
2015-08-29 12:19:56 -0700

    msm: mdss: KCAL: Send a pp display commit when changes are made
    Sending a PP display commit when changes are made, to prevent waiting on a panel update from
    some other part of the kernel. Sending a display commit takes care of locking and panel state
    checking for us.
    
    QC MDSS also handles queue-ing already for us, so remove that functionality from KCAL.
    
    Change-Id: I9a12675e633f926d03f52bf78a4dacb133ccbed4
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
+++ drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -383,7 +383,7 @@ static ssize_t kcal_enable_store(struct device *dev,
 
 	mdss_mdp_kcal_update_pcc(lut_data);
 	mdss_mdp_kcal_update_pa(lut_data);
-	//mdss_mdp_kcal_update_igc(lut_data);
+	mdss_mdp_kcal_update_igc(lut_data);
 	mdss_mdp_kcal_display_commit();
 
 	return count;
@@ -411,8 +411,9 @@ static ssize_t kcal_invert_store(struct device *dev,
 	//disable
 	lut_data->invert = 0;
 
-	//mdss_mdp_kcal_update_igc(lut_data);
-	//mdss_mdp_kcal_display_commit();
+	mdss_mdp_kcal_update_igc(lut_data);
+	mdss_mdp_kcal_display_commit();
+
 
 	return count;
 }
