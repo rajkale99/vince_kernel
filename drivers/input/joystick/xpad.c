34ca7197cd1c89cf9703dd0714e83491be73d06f
Pavel Rojtberg <rojtberg@gmail.com>
2015-10-10 10:00:49 -0700

    Input: xpad - use ida() for finding the pad_nr
    [ Upstream commit e3b651745a03810efffd7ebf2a9b5be65fb70ec3 ]
    
    The pad_nr corresponds to the lit up LED on the controller. Therefore there
    should be no gaps when enumerating. Currently a LED is only re-assigned
    after a controller is re-connected 4 times.
    
    This patch uses ida to track connected pads - this way we can re-assign
    freed up pad number immediately.
    
    Consider the following case:
    1. pad A is connected and gets pad_nr = 0
    2. pad B is connected and gets pad_nr = 1
    3. pad A is disconnected
    4. pad A is connected again
    
    using ida_simple_get() controller A now correctly gets pad_nr = 0 again.
    
    Signed-off-by: Pavel Rojtberg <rojtberg@gmail.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/input/joystick/xpad.c
+++ drivers/input/joystick/xpad.c
@@ -343,8 +343,7 @@ struct usb_xpad {
 
 	int mapping;			/* map d-pad to buttons or to axes */
 	int xtype;			/* type of xbox device */
-
-	unsigned long pad_nr;		/* the order x360 pads were attached */
+	int pad_nr;			/* the order x360 pads were attached */
 };
 
 /*
@@ -982,7 +981,6 @@ static void xpad_led_set(struct led_classdev *led_cdev,
 static int xpad_led_probe(struct usb_xpad *xpad)
 {
 
-	static atomic_t led_seq = ATOMIC_INIT(-1);
 	struct xpad_led *led;
 	struct led_classdev *led_cdev;
 	int error;
@@ -994,10 +992,14 @@ static int xpad_led_probe(struct usb_xpad *xpad)
 	if (!led)
 		return -ENOMEM;
 
-	xpad->led_no = atomic_inc_return(&led_seq);
-	xpad->pad_nr = atomic_inc_return(&led_seq);
 
-	snprintf(led->name, sizeof(led->name), "xpad%lu", xpad->pad_nr);
+	xpad->pad_nr = ida_simple_get(&xpad_pad_seq, 0, 0, GFP_KERNEL);
+	if (xpad->pad_nr < 0) {
+		error = xpad->pad_nr;
+		goto err_free_mem;
+	}
+
+	snprintf(led->name, sizeof(led->name), "xpad%d", xpad->pad_nr);
 	led->xpad = xpad;
 
 	led_cdev = &led->led_cdev;
@@ -1010,7 +1012,6 @@ static int xpad_led_probe(struct usb_xpad *xpad)
 
 	/* Light up the segment corresponding to controller number */
 	xpad_identify_controller(xpad);
-
 	return 0;
 
 err_free_id:
