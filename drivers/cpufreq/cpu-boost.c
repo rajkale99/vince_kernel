51dc8ec63a51b15aab6e43916afb1c3ddf1cdd00
Blacksuan19 <abubakaryagob@gmail.com>
2018-05-06 19:42:10 +0700

    cpu-boost: update default values
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -36,7 +36,9 @@ static struct workqueue_struct *cpu_boost_wq;
 static struct work_struct input_boost_work;
 static bool input_boost_enabled;
 
-static unsigned int input_boost_ms = 1500;
+
+static unsigned int input_boost_ms = 900;
+
 module_param(input_boost_ms, uint, 0644);
 
 static bool sched_boost_on_input;
@@ -379,8 +381,7 @@ static int cpu_boost_init(void)
 	for_each_possible_cpu(cpu) {
 		s = &per_cpu(sync_info, cpu);
 		s->cpu = cpu;
-		s->input_boost_freq = 1036800;
-
+		s->input_boost_freq = 1401600;
 	}
 	cpufreq_register_notifier(&boost_adjust_nb, CPUFREQ_POLICY_NOTIFIER);
 	ret = input_register_handler(&cpuboost_input_handler);
